becfd59f0fc2e642f5dd8768a5379eb573d1fa87
Denys Leon <leonden0046@gmail.com>
2026-01-29 11:29:42 +0200

    revert

--- GameEngine/main.cpp
+++ GameEngine/main.cpp
@@ -20,6 +20,9 @@ const GLint SCREEN_HEIGHT = 600;
 
 GLuint VAO;
 GLuint VBO;
+GLuint FBO;
+GLuint RBO;
+GLuint textureId;
 GLuint shaderProgramId;
 
 const char* vertexShader = R"glsl(
@@ -45,6 +48,9 @@ void main() {
 void createTriangle();
 void addShader(GLuint shaderProgram, const char* shaderCode, GLenum shaderType);
 void createAndCompileShader();
+
+void createFramebuffer();
+void rescaleFramebuffer(float width, float height);
 
 int main() {
 
@@ -87,6 +93,7 @@ int main() {
 
 	createTriangle();
 	createAndCompileShader();
+	createFramebuffer();
 
 	////Init ImGui
 	IMGUI_CHECKVERSION();
@@ -109,8 +116,16 @@ int main() {
 
 		// ImGui Window
 		ImGui::Begin("Game Engine");
-		ImGui::ShowDemoWindow();
 
+		const float windowWidth = ImGui::GetContentRegionAvail().x;
+		const float windowHeight = ImGui::GetContentRegionAvail().y;
+
+		// Render to framebuffer FIRST (before using the texture in ImGui)
+		glBindFramebuffer(GL_FRAMEBUFFER, FBO);
+		rescaleFramebuffer(windowWidth, windowHeight);
+		glViewport(0, 0, static_cast<GLsizei>(windowWidth), static_cast<GLsizei>(windowHeight));
+
+		// Clear the framebuffer
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -123,11 +138,30 @@ int main() {
 
 		glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
+		// Now display the framebuffer texture in ImGui
+		ImVec2 pos = ImGui::GetCursorScreenPos();
+
+		ImGui::GetWindowDrawList()->AddImage(
+			(void*)(intptr_t)textureId,
+			ImVec2(pos.x, pos.y),
+			ImVec2(pos.x + windowWidth, pos.y + windowHeight),
+			ImVec2(0, 1),
+			ImVec2(1, 0)
+		);
+
 		ImGui::End();
 
 		// Render ImGui
 		ImGui::Render();
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+		{
+			GLFWwindow* backup_current_context = glfwGetCurrentContext();
+			ImGui::UpdatePlatformWindows();
+			ImGui::RenderPlatformWindowsDefault();
+			glfwMakeContextCurrent(backup_current_context);
+		}
 
 		glfwSwapBuffers(mainWindow);
 		glfwPollEvents();
@@ -219,6 +253,42 @@ void createAndCompileShader() {
 		std::cout << "Error validating program:\n" << eLog << '\n';
 		return;
 	}
+}
+
+void createFramebuffer() {
+	glGenFramebuffers(1, &FBO);
+	glBindFramebuffer(GL_FRAMEBUFFER, FBO);
+
+	glGenTextures(1, &textureId);
+	glBindTexture(GL_TEXTURE_2D, textureId);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, SCREEN_WIDTH, SCREEN_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureId, 0);
+
+	glGenRenderbuffers(1, &RBO);
+	glBindRenderbuffer(GL_RENDERBUFFER, RBO);
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, SCREEN_WIDTH, SCREEN_HEIGHT);
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, RBO);
+
+	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+		std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!\n";
+
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glBindRenderbuffer(GL_RENDERBUFFER, 0);
+}
+
+void rescaleFramebuffer(float width, float height) {
+	glBindTexture(GL_TEXTURE_2D, textureId);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureId, 0);
+
+	glBindRenderbuffer(GL_RENDERBUFFER, RBO);
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, width, height);
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, RBO);
 }
 
 void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
